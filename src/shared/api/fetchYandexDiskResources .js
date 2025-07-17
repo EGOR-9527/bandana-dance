e8a6276699df8d038a1b2eceb0dd18024986a85c
Egor Docenko <egordocenko359@gmail.com>
2025-07-18 02:15:32 +0300

    Сука давай блят сука давай гандон

--- src/shared/api/fetchYandexDiskResources .js
+++ src/shared/api/fetchYandexDiskResources .js
@@ -2,27 +2,18 @@ import axios from "axios";
 
 const baseURL = "https://cloud-api.yandex.net/v1/disk/public/resources";
 
-const DEFAULT_HEADERS = {
-  Referer: "https://bandana-dance.ru/",
-  "User-Agent":
-    "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Mobile Safari/537.36",
-  Accept: "*/*",
-};
-
 export const fetchFiles = async (publicKey) => {
   try {
-    // Получаем список файлов из публичной папки
     const res = await axios.get(baseURL, {
       params: {
         public_key: publicKey,
         path: "",
       },
-      headers: DEFAULT_HEADERS,
+      // без headers!
     });
 
     const items = res.data._embedded?.items || [];
 
-    // Запрашиваем ссылку на скачивание для каждого файла
     const filesWithDownloadLinks = await Promise.all(
       items.map(async (item) => {
         const downloadRes = await axios.get(
@@ -32,8 +23,6 @@ export const fetchFiles = async (publicKey) => {
               public_key: publicKey,
               path: item.path,
             },
-            headers: DEFAULT_HEADERS,
-            responseType: "stream",
           }
         );
 
@@ -46,10 +35,11 @@ export const fetchFiles = async (publicKey) => {
 
     return filesWithDownloadLinks;
   } catch (error) {
-    console.error("Error fetching files:", error);
+    console.error("Error fetching files:", error.response?.data || error.message);
     return [];
   }
 };
+
 
 // Функция, возвращающая сразу только изображения с download_url
 export const fetchImg = async () => {
