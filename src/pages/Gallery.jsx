b5caa50e19bf90248313d7bf31b73374880d24a4
Egor Docenko <egordocenko359@gmail.com>
2025-12-28 21:47:19 +0300

    добавил контакты на все страницы

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import { ParticleCanvas } from "../shared/ui/Canvas/ParticleCanvas";
 import Header from "../widget/Header/ui/Header.jsx";
 import FilteredPhotos from "../features/ExpandableText/ui/FilteredPhotos.jsx";
-import ScrollProgress from "../widget/ScrollProgress/ui/ScrollProgress.jsx"
+import ScrollProgress from "../widget/ScrollProgress/ui/ScrollProgress.jsx";
+import Contact from "../widget/Contact/ui/Contact.jsx";
+
 const Gallery = () => {
   return (
     <div>
       <ParticleCanvas className="background" />
-      <Header text={[{page: "Главная страница", link: "/"}, {page: "Команды", link: "/teams"}]} />
+      <Header
+        text={[
+          { page: "Главная страница", link: "/" },
+          { page: "Команды", link: "/teams" },
+        ]}
+      />
       <FilteredPhotos />
-      <ScrollProgress/>
+      <ScrollProgress />
+      <Contact />
     </div>
   );
 };
--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { ParticleCanvas } from "../shared/ui/Canvas/ParticleCanvas";
 import Header from "../widget/Header/ui/Header.jsx";
-import TeamsCarts from "../widget/Teams/ui/TeamsCarts.jsx"
+import TeamsCarts from "../widget/Teams/ui/TeamsCarts.jsx";
+import Contact from "../widget/Contact/ui/Contact.jsx";
 const Teams = () => {
   return (
     <div>
       <ParticleCanvas className="background" />
-      <Header text={[{page: "Главная страница", link: "/"}, {page: "Галерея", link: "/gallery"}]} />
-      <TeamsCarts/>
+      <Header
+        text={[
+          { page: "Главная страница", link: "/" },
+          { page: "Галерея", link: "/gallery" },
+        ]}
+      />
+      <TeamsCarts />
+      <Contact/>
     </div>
   );
 };
